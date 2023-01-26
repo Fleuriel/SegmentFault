47f80c276d02aec72701350e589cdf0546e7314a
Fleuriel <tan.a@digipen.edu>
2023-01-26 12:53:04 +0800

    PUSH1
    PUSH2

--- SEP2_Project/SEP2_Project/Main.cpp
+++ SEP2_Project/SEP2_Project/Main.cpp
@@ -33,23 +33,39 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 	//My Own Testing Codes
 	AEGfxVertexList* pMesh = 0;
-	//AEGfxTexture* pTex = AEGfxTextureLoad("Assets/PlanetTexture.png");
-	AEGfxTexture* pTex = AEGfxTextureLoad("Assets/Rainbow1.png");
+	AEGfxVertexList* oOMesh = 0;
+	AEGfxTexture* pTex = AEGfxTextureLoad("Assets/Assets/PlanetTexture.png");
+	AEGfxTexture* oOTex = AEGfxTextureLoad("Assets/Assets/images.png");
 
 	AEGfxMeshStart();
 
 	AEGfxTriAdd(-0.5f, -0.5f, 0xFFFF00FF, 0.0f, 0.0f,
-				 0.5f, -0.5f, 0xFFFFFF00, 1.0f, 0.0f,
-				 -0.5f, 0.5f, 0xFF00FFFF, 0.0f, 1.0f);
+		0.5f, -0.5f, 0xFFFFFF00, 1.0f, 0.0f,
+		-0.5f, 0.5f, 0xFF00FFFF, 0.0f, 1.0f);
 
-	AEGfxTriAdd( 0.5f, -0.5f, 0xFFFFFFFF, 1.0f, 0.0f,
-				 0.5f, 0.5f, 0xFFFFFFFF, 1.0f, 1.0f,
-				 -0.5f, 0.5f, 0xFFFFFFFF, 0.0f, 1.0f);
-
-
+	AEGfxTriAdd(0.5f, -0.5f, 0xFFFFFFFF, 1.0f, 0.0f,
+		0.5f, 0.5f, 0xFFFFFFFF, 1.0f, 1.0f,
+		-0.5f, 0.5f, 0xFFFFFFFF, 0.0f, 1.0f);
 	pMesh = AEGfxMeshEnd();
 
-	f32 x = 0.0f , y = 0.0f, r = 0.0f, z = 0.0f;
+	AEGfxMeshStart();
+
+	AEGfxTriAdd(0.0f, 0.0f, 0xFFFF00FF, 0.0f, 0.0f,
+		1.0f, 0.0f, 0xFFFFFF00, 1.0f, 0.0f,
+		0.0f, 1.0f, 0xFF00FFFF, 0.0f, 1.0f);
+
+	AEGfxTriAdd(1.0f, 0.0f, 0xFFFFFFFF, 1.0f, 0.0f,
+		1.0f, 1.0f, 0xFFFFFFFF, 1.0f, 1.0f,
+		0.0f, 1.0f, 0xFFFFFFFF, 0.0f, 1.0f);
+
+
+	oOMesh = AEGfxMeshEnd();
+
+
+
+
+
+	f32 x = 0.0f , y = 0.0f, r = 0.0f, z = 0.0f,x1 = 50.0f, y1  = 50.0f;
 	// Game Loop
 
 	while (gGameRunning)
@@ -60,9 +76,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		// Handling Input
 		AEInputUpdate();
 
+
+	
 		if (AEInputCheckCurr(AEVK_RIGHT))
 		{
 			x += 5.0f;
+			x1 += 5.0f;
 			std::cout << "x: " << x << ' ' << "y: " << y << ' ' << "z: " << z << ' ' << "r: " << r << '\n';
 			
 			//			y = x;
@@ -71,17 +90,20 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		{
 			x -= 5.0f;
 
+			x1 -= 5.0f;
 			std::cout << "x: " << x << ' ' << "y: " << y << ' ' << "z: " << z << ' ' << "r: " << r << '\n';
 			//			y = x;
 		}		
 		if (AEInputCheckCurr(AEVK_DOWN))
 		{
 			y -= 5.0f;
+			y1 -= 5.0f;
 			std::cout << "x: " << x << ' ' << "y: " << y << ' ' << "z: " << z << ' ' << "r: " << r << '\n';
 		}		
 		if (AEInputCheckCurr(AEVK_UP))
 		{
 			y += 5.0f;
+			y1 += 5.0f;
 			std::cout << "x: " << x << ' ' << "y: " << y << ' ' << "z: " << z << ' ' << "r: " << r << '\n';
 		}
 		if (AEInputCheckCurr(AEVK_A))
@@ -132,7 +154,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 		// Create a scale matrix that scales by 100 x and y
 		AEMtx33 scale = { 0 };
-		AEMtx33Scale(&scale,100 ,100);
+		AEMtx33Scale(&scale,z ,z);
 		// Create a rotation matrix that rotates by 45 degrees
 		AEMtx33 rotate = { 0 };
 		AEMtx33Rot(&rotate, r);
@@ -154,19 +176,51 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 		// Your own rendering logic goes here
 
+		// Set the background to black.
+		AEGfxSetBackgroundColor(0.0f, 0.0f, 0.0f);
+		// Tell the engine to get ready to draw something with texture.
+		AEGfxSetRenderMode(AE_GFX_RM_TEXTURE);
+		// Set the tint to white, so that the sprite can
+		// display the full range of colors (default is black).
+		AEGfxSetTintColor(1.0f, 1.0f, 1.0f, 1.0f);
+		// Set blend mode to AE_GFX_BM_BLEND
+		// This will allow transparency.
+		AEGfxSetBlendMode(AE_GFX_BM_BLEND);
+		AEGfxSetTransparency(1.0f);
 
+		AEGfxTextureSet(oOTex, 0, 0);
+		AEMtx33 scale1 = { 0 };
+		AEMtx33Scale(&scale1, z, z);
+		// Create a rotation matrix that rotates by 45 degrees
+		AEMtx33 rotate1 = { 0 };
+		AEMtx33Rot(&rotate1, r);
+		// Create a translation matrix that translates by
+		// 100 in the x-axis and 100 in the y-axis
+		AEMtx33 translate1 = { 0 };
+		AEMtx33Trans(&translate1, x, y);
+		// Concat the matrices (TRS)
+		AEMtx33 transform1 = { 0 };
+		AEMtx33Concat(&transform1, &rotate1, &scale1);
+		AEMtx33Concat(&transform1, &translate1, &transform1);
+		// Choose the transform to use
+		AEGfxSetTransform(transform1.m);
+		// Actually drawing the mesh
+		AEGfxMeshDraw(oOMesh, AE_GFX_MDM_TRIANGLES);
 		// Informing the system about the loop's end
-		AESysFrameEnd();
 
+		AESysFrameEnd();
 		// check if forcing the application to quit
 		if (AEInputCheckTriggered(AEVK_ESCAPE) || 0 == AESysDoesWindowExist())
 			gGameRunning = 0;
 	}
 
 	AEGfxMeshFree(pMesh);
+	AEGfxMeshFree(oOMesh);
 
-	
 	AEGfxTextureUnload(pTex);
+	AEGfxTextureUnload(oOTex);
+
+
 	// free the system
 	AESysExit();
 }
