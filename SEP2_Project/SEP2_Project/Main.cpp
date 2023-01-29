ee6ba875d685c214387bc4ef0b5499a7f134c59d
Fleuriel <tan.a@digipen.edu>
2023-01-30 00:21:44 +0800

    Push
    Push

--- SEP2_Project/SEP2_Project/Main.cpp
+++ SEP2_Project/SEP2_Project/Main.cpp
@@ -1,13 +1,17 @@
 // ---------------------------------------------------------------------------
 // includes
 
+#define MAX_ENEMIES 10
+
 #include "AEEngine.h"
 #include <iostream>
 #include <math.h>
-
-
+#include "enemy.hpp"
+#include <vector>
+#include <random>
 // ---------------------------------------------------------------------------
-// main
+
+
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -16,15 +20,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
-
-
 	int gGameRunning = 1;
-	
-
 	// Initialization of your own variables go here
-
 	// Using custom window procedure
-	AESysInit(hInstance, nCmdShow, 1366, 768, 1, 60, true, NULL);
+	AESysInit(hInstance, nCmdShow, 800, 600, 1, 60, true, NULL);
 
 	// Changing the window title
 	AESysSetWindowTitle("Among Them");
@@ -32,46 +31,38 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	// reset the system modules
 	AESysReset();
 
+	int array[MAX_ENEMIES] = {};
+	int distanceX[MAX_ENEMIES] = {};
+	int distanceY[MAX_ENEMIES] = {};
 	//My Own Testing Codes
 	AEGfxVertexList* pMesh = 0;
 	AEGfxVertexList* oOMesh = 0;
-	AEGfxTexture* pTex = AEGfxTextureLoad("Assets/Assets/PlanetTexture.png");
-	AEGfxTexture* oOTex = AEGfxTextureLoad("Assets/Assets/TrollFace.png");
+	AEGfxTexture* pTex = AEGfxTextureLoad("Assets/Assets/TrollFace.png");
+	AEGfxTexture* oOTex = AEGfxTextureLoad("Assets/Assets/moon.png");
+	AEGfxTexture* ETex = AEGfxTextureLoad("Assets/Assets/images.png");
 
 	AEGfxMeshStart();
-
 	AEGfxTriAdd(-0.5f, -0.5f, 0xFFFF00FF, 0.0f, 0.0f,
 		0.5f, -0.5f, 0xFFFFFF00, 1.0f, 0.0f,
 		-0.5f, 0.5f, 0xFF00FFFF, 0.0f, 1.0f);
-
 	AEGfxTriAdd(0.5f, -0.5f, 0xFFFFFFFF, 1.0f, 0.0f,
 		0.5f, 0.5f, 0xFFFFFFFF, 1.0f, 1.0f,
 		-0.5f, 0.5f, 0xFFFFFFFF, 0.0f, 1.0f);
 	pMesh = AEGfxMeshEnd();
-
 	AEGfxMeshStart();
 
-	AEGfxTriAdd(0.0f, 0.0f, 0xFFFF00FF, 0.0f, 0.0f,
-		1.0f, 0.0f, 0xFFFFFF00, 1.0f, 0.0f,
-		0.0f, 1.0f, 0xFF00FFFF, 0.0f, 1.0f);
-
-	AEGfxTriAdd(1.0f, 0.0f, 0xFFFFFFFF, 1.0f, 0.0f,
-		1.0f, 1.0f, 0xFFFFFFFF, 1.0f, 1.0f,
-		0.0f, 1.0f, 0xFFFFFFFF, 0.0f, 1.0f);
 
 
-	oOMesh = AEGfxMeshEnd();
-
-
-
-
-
+	int enemySpeed = 1;
+	f32 x = 0.0f, y = 0.0f;
 	f32 rotationx = 0;
-	f32 x = 0.0f , y = 0.0f, r = 0.0f, z = 0.0f,x1 = 50.0f, y1  = 50.0f;
+	f32 r = 100.0f, z = 0.0f,x1 = 50.0f, y1  = 50.0f;
 	// Game Loop
 
-	f32 enemyX = 0.0f, enemyY = 0.0f;
+	f32 enemyX = 200.0f, enemyY = 200.0f;
 
+
+	f32 distance = 0.0f;
 	while (gGameRunning)
 	{
 		// Informing the system about the loop's start
@@ -80,21 +71,36 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		// Handling Input
 		AEInputUpdate();
 
+		for (int i = 0; i < MAX_ENEMIES; i++)
+		{
 
+
+			float distanceX = enemyX - x;
+			float distanceY = enemyY - y;
+
+			float magnitude = sqrt(pow(distanceX, 2) + pow(distanceY, 2));
+			distanceX /= magnitude;
+			distanceY /= magnitude;
+
+			enemyX -= distanceX * enemySpeed;
+			enemyY -= distanceY * enemySpeed;
+		}
 	
+		
+
+		//std::cout << distance << '\n';
+
+//		std::cout << "x1: " << x1 << ' ' << "y1: " << y1 << '\n';
+
 		if (AEInputCheckCurr(AEVK_RIGHT))
 		{
 			x += 5.0f;
 			std::cout << "x: " << x << ' ' << "y: " << y << ' ' << "z: " << z << ' ' << "r: " << r << '\n';
-			
-			//			y = x;
 		}
 		if (AEInputCheckCurr(AEVK_LEFT))
 		{
 			x -= 5.0f;
-
 			std::cout << "x: " << x << ' ' << "y: " << y << ' ' << "z: " << z << ' ' << "r: " << r << '\n';
-			//			y = x;
 		}		
 		if (AEInputCheckCurr(AEVK_DOWN))
 		{
@@ -109,7 +115,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		if (AEInputCheckCurr(AEVK_A))
 		{
 			r -= 0.01f;
-
 			std::cout << "x: " << x << ' ' << "y: " << y << ' ' << "z: " << z << ' ' << "r: " << r << '\n';
 		}		
 		if (AEInputCheckCurr(AEVK_D))
@@ -120,19 +125,16 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		if (AEInputCheckCurr(AEVK_1))
 		{
 			z -= 1.0f;
-
 			std::cout << "x: " << x << ' ' << "y: " << y << ' ' << "z: " << z << ' ' << "r: " << r << '\n';
 		}
 		if (AEInputCheckCurr(AEVK_2))
 		{
 			z += 1.0f;
-
 			std::cout << "x: " << x << ' ' << "y: " << y << ' ' << "z: " << z << ' ' << "r: " << r << '\n';
 		}
 		if (AEInputCheckCurr(AEVK_R))
 		{
 			z = 100.0f;
-
 			std::cout << "x: " << x << ' ' << "y: " << y << ' ' << "z: " << z << ' ' << "r: " << r << '\n';
 		}
 
@@ -166,7 +168,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		AEMtx33Scale(&scale,z ,z);
 		// Create a rotation matrix that rotates by 45 degrees
 		AEMtx33 rotate = { 0 };
-		AEMtx33Rot(&rotate, r);
+		AEMtx33Rot(&rotate, 135);
 		// Create a translation matrix that translates by
 		// 100 in the x-axis and 100 in the y-axis
 		AEMtx33 translate = { 0 };
@@ -183,8 +185,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 		//let this one be the turning ones.
 
-
-
+	
 		AEGfxTextureSet(oOTex, 0, 0);
 		AEMtx33 scale1 = { 0 };
 		AEMtx33Scale(&scale1, 50, 50);
@@ -202,13 +203,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		// Choose the transform to use
 		AEGfxSetTransform(transform1.m);
 		// Actually drawing the mesh
-		AEGfxMeshDraw(oOMesh, AE_GFX_MDM_TRIANGLES);
-		// Informing the system about the loop's end
+		AEGfxMeshDraw(pMesh, AE_GFX_MDM_TRIANGLES);
+		
 
 
-
-
-
+		std::cout << "enemyX: " << enemyX << "  enemyY: " << enemyY << '\n';
 		/*		A
 			   /|
 			  / |
@@ -218,6 +217,21 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		*/
 
 
+		AEGfxTextureSet(ETex, 0, 0);
+		AEMtx33Scale(&scale1, 50, 50);
+		// Create a rotation matrix that rotates by 45 degrees
+		AEMtx33Rot(&rotate1, rotationx);
+		// Create a translation matrix that translates by
+		// 100 in the x-axis and 100 in the y-axis
+		AEMtx33Trans(&translate1, enemyX, enemyY);
+		// Concat the matrices (TRS)
+		
+		AEMtx33Concat(&transform1, &rotate1, &scale1);
+		AEMtx33Concat(&transform1, &translate1, &transform1);
+		// Choose the transform to use
+		AEGfxSetTransform(transform1.m);
+		// Actually drawing the mesh
+		AEGfxMeshDraw(pMesh, AE_GFX_MDM_TRIANGLES);
 
 		AESysFrameEnd();
 		// check if forcing the application to quit
@@ -226,11 +240,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	}
 
 	AEGfxMeshFree(pMesh);
-	AEGfxMeshFree(oOMesh);
 
 	AEGfxTextureUnload(pTex);
 	AEGfxTextureUnload(oOTex);
 
+	AEGfxTextureUnload(ETex);
 
 	// free the system
 	AESysExit();
