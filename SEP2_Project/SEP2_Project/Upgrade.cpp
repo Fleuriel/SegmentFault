b10af61d0a96b642a320b332a398710936010d53
EmeryQuek <c.quek@digipen.edu>
2023-03-13 20:43:54 +0800

    Upgrade.cpp updates

--- SEP2_Project/SEP2_Project/Upgrade.cpp
+++ SEP2_Project/SEP2_Project/Upgrade.cpp
@@ -6,10 +6,13 @@
 
 //Pointer to Mesh
 AEGfxVertexList* pMeshUpgrade = nullptr;
+AEGfxVertexList* pMeshUpgrade1 = nullptr;
 
-// Pre-definition for translations of buttons
+// Pre-definition for translations
 double UpgradebackButton_transX;
 double UpgradebackButton_transY;
+double UpgradeStatDisplay_transX;
+double UpgradeStatDisplay_transY;
 
 // Pre-definition of scaling
 double UpgradescaleX_settings;
@@ -22,7 +25,7 @@ char Upgrade1_buffer[1024]{};
 
 void Upgrade_Load(void) 
 {
-	std::cout << "Upgrade_Load\n";
+	//std::cout << "Upgrade_Load\n";
 	AEGfxSetBackgroundColor(0.329f, 0.408f, 0.471f);
 
 	AEGfxMeshStart();
@@ -38,6 +41,20 @@ void Upgrade_Load(void)
 
 	pMeshUpgrade = AEGfxMeshEnd();
 
+
+	AEGfxMeshStart();
+
+	AEGfxTriAdd(
+		0.5f, 0.5f, 0xFF001736, 1.0f, 0.0f,
+		-1.2f, -0.5f, 0xFF001736, 0.0f, 1.0f,
+		0.5f, -0.5f, 0xFF001736, 1.0f, 1.0f);
+	AEGfxTriAdd(
+		-1.2f, 0.5f, 0xFF001736, 0.0f, 0.0f,
+		-1.2f, -0.5f, 0xFF001736, 0.0f, 1.0f,
+		0.5f, 0.5f, 0xFF001736, 1.0f, 0.0f);
+
+	pMeshUpgrade1 = AEGfxMeshEnd();
+
 }
 
 void Upgrade_Init(void) 
@@ -52,6 +69,9 @@ void Upgrade_Init(void)
 	// Back button translation
 	UpgradebackButton_transX = 640 * UpgradescaleX_settings;
 	UpgradebackButton_transY = -355.f * UpgradescaleY_settings;
+
+	UpgradeStatDisplay_transX = 33 * UpgradescaleX_settings;
+	UpgradeStatDisplay_transY = 340.f * UpgradescaleY_settings;
 
 }
 void Upgrade_Update(void) 
@@ -74,8 +94,7 @@ void Upgrade_Update(void)
 	float backButton_midX = (getWinWidth() / 2.08) + UpgradebackButton_transX;
 	float backButton_midY = (getWinHeight() / 2) - UpgradebackButton_transY;
 
-	//printf("%d\n%d\n", cursorX, cursorY);  //debug
-	//printf("%f\n%f\n", button1_midX, button1_midY);  //debug
+
 	// if cursor within buttons, change game state
 	if (IsAreaClicked(backButton_midX, backButton_midY, 150.0f * UpgradescaleX_settings, 100.0f * UpgradescaleY_settings, cursorX, cursorY)
 		&& AEInputCheckTriggered(AEVK_LBUTTON)) {
@@ -86,7 +105,7 @@ void Upgrade_Update(void)
 }
 void Upgrade_Draw(void) 
 {
-	// Back button
+	// Back word box
 	AEGfxTextureSet(NULL, 0, 0);
 	AEGfxSetRenderMode(AE_GFX_RM_COLOR);
 	AEMtx33 scale = { 0 };
@@ -101,6 +120,52 @@ void Upgrade_Draw(void)
 	AEGfxSetTransform(transform.m);
 	AEGfxMeshDraw(pMeshUpgrade, AE_GFX_MDM_TRIANGLES);
 
+	// Upgrades word box
+	AEGfxTextureSet(NULL, 0, 0);
+	AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+	AEMtx33 scale1 = { 0 };
+	AEMtx33Scale(&scale1, 120.f * UpgradescaleX_settings, 60.f * UpgradescaleY_settings);
+	AEMtx33 rotate1 = { 0 };
+	AEMtx33Rot(&rotate1, 0.f);
+	AEMtx33 translate1 = { 0 };
+	AEMtx33Trans(&translate1, UpgradeStatDisplay_transX, UpgradeStatDisplay_transY);
+	AEMtx33 transform1 = { 0 };
+	AEMtx33Concat(&transform1, &rotate1, &scale1);
+	AEMtx33Concat(&transform1, &translate1, &transform1);
+	AEGfxSetTransform(transform1.m);
+	AEGfxMeshDraw(pMeshUpgrade1, AE_GFX_MDM_TRIANGLES);
+
+	// Ship Models word box
+	AEGfxTextureSet(NULL, 0, 0);
+	AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+	AEMtx33 scale2 = { 0 };
+	AEMtx33Scale(&scale2, 150.f * UpgradescaleX_settings, 60.f * UpgradescaleY_settings);
+	AEMtx33 rotate2 = { 0 };
+	AEMtx33Rot(&rotate2, 0.f);
+	AEMtx33 translate2 = { 0 };
+	AEMtx33Trans(&translate2, UpgradeStatDisplay_transX - 350.f , UpgradeStatDisplay_transY -120.f);
+	AEMtx33 transform2 = { 0 };
+	AEMtx33Concat(&transform2, &rotate2, &scale2);
+	AEMtx33Concat(&transform2, &translate2, &transform2);
+	AEGfxSetTransform(transform2.m);
+	AEGfxMeshDraw(pMeshUpgrade1, AE_GFX_MDM_TRIANGLES);
+
+	// Stats word box
+	AEGfxTextureSet(NULL, 0, 0);
+	AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+	AEMtx33 scale3 = { 0 };
+	AEMtx33Scale(&scale3, 80.f * UpgradescaleX_settings, 60.f * UpgradescaleY_settings);
+	AEMtx33 rotate3 = { 0 };
+	AEMtx33Rot(&rotate3, 0.f);
+	AEMtx33 translate3 = { 0 };
+	AEMtx33Trans(&translate3, UpgradeStatDisplay_transX + 200.f, UpgradeStatDisplay_transY - 120.f);
+	AEMtx33 transform3 = { 0 };
+	AEMtx33Concat(&transform3, &rotate3, &scale3);
+	AEMtx33Concat(&transform3, &translate3, &transform3);
+	AEGfxSetTransform(transform3.m);
+	AEGfxMeshDraw(pMeshUpgrade1, AE_GFX_MDM_TRIANGLES);
+
+	// "Back" Text
 	AEGfxSetRenderMode(AE_GFX_RM_COLOR);
 	AEGfxTextureSet(NULL, 0, 0);
 	AEGfxSetBlendMode(AE_GFX_BM_BLEND);
@@ -108,12 +173,29 @@ void Upgrade_Draw(void)
 	AEGfxGetPrintSize(fontID, Upgrade1_buffer, 1.0f, Upgrade_textWidth, Upgrade_textHeight);
 	AEGfxPrint(fontID, Upgrade1_buffer, (getWinWidth() / (1655.f * UpgradescaleX_settings)), (getWinHeight() / (-798.f * UpgradescaleY_settings)), 0.8f * UpgradescaleX_settings, 156.0f / 255.f, 205.0f / 255.f, 220.0f / 255.f);
 
+	// "Upgrades" Text
 	AEGfxSetRenderMode(AE_GFX_RM_COLOR);
 	AEGfxTextureSet(NULL, 0, 0);
 	AEGfxSetBlendMode(AE_GFX_BM_BLEND);
 	sprintf_s(Upgrade1_buffer, "Upgrades");
 	AEGfxGetPrintSize(fontID, Upgrade1_buffer, 1.0f, Upgrade_textWidth, Upgrade_textHeight);
 	AEGfxPrint(fontID, Upgrade1_buffer, (getWinWidth() / (-9000.f * UpgradescaleX_settings)), (getWinHeight() / (900.f * UpgradescaleY_settings)), 0.8f * UpgradescaleX_settings, 156.0f / 255.f, 205.0f / 255.f, 220.0f / 255.f);
+
+	// "Ship Models" Text
+	AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+	AEGfxTextureSet(NULL, 0, 0);
+	AEGfxSetBlendMode(AE_GFX_BM_BLEND);
+	sprintf_s(Upgrade1_buffer, "Ship Models");
+	AEGfxGetPrintSize(fontID, Upgrade1_buffer, 1.0f, Upgrade_textWidth, Upgrade_textHeight);
+	AEGfxPrint(fontID, Upgrade1_buffer, (getWinWidth() / (-1960.f * UpgradescaleX_settings)), (getWinHeight() / (1450.f * UpgradescaleY_settings)), 0.8f * UpgradescaleX_settings, 156.0f / 255.f, 205.0f / 255.f, 220.0f / 255.f);
+
+	AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+	AEGfxTextureSet(NULL, 0, 0);
+	AEGfxSetBlendMode(AE_GFX_BM_BLEND);
+	sprintf_s(Upgrade1_buffer, "Stats");
+	AEGfxGetPrintSize(fontID, Upgrade1_buffer, 1.0f, Upgrade_textWidth, Upgrade_textHeight);
+	AEGfxPrint(fontID, Upgrade1_buffer, (getWinWidth() / (6100.f * UpgradescaleX_settings)), (getWinHeight() / (1450.f * UpgradescaleY_settings)), 0.8f * UpgradescaleX_settings, 156.0f / 255.f, 205.0f / 255.f, 220.0f / 255.f);
+
 }
 void Upgrade_Free(void) 
 {
