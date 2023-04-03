bb15b293e91b71adb9e12d9787bf49ec49b0ab62
Aloysius Ang <122081213+Umidurr@users.noreply.github.com>
2023-04-03 19:57:48 +0800

    added documentation for gameovermenu

--- SEP2_Project/SEP2_Project/GameOverMenu.h
+++ SEP2_Project/SEP2_Project/GameOverMenu.h
@@ -1,3 +1,16 @@
+/******************************************************************************/
+/*!
+\file		GameOverMenu.h
+\author		Ang Jun Sheng Aloysius, a.junshengaloysius, 2201807
+\par		a.junshengaloysius@digipen.edu
+\brief		Contains the declarations of functions that allows players to continue or go back to title screen.
+
+Copyright (C) 2023 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+ */
+ /******************************************************************************/
 #pragma once
 
 void GameOver_Load(void);
