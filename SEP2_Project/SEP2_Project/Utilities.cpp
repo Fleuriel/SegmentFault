2438f343f4b748b301f040fe173c5833ec9671ce
Aloysius Ang <122081213+Umidurr@users.noreply.github.com>
2023-04-03 22:29:24 +0800

    added documentation for utilities

--- SEP2_Project/SEP2_Project/Utilities.cpp
+++ SEP2_Project/SEP2_Project/Utilities.cpp
@@ -141,7 +141,27 @@ bool CollisionCircleCircle(const AEVec2& center1, f32 radius1, const AEVec2& cen
 	return distanceSquared <= radiusSum * radiusSum;
 }
 
-// Function to check if the area is being touched/clicked
+/*!*****************************************************************
+  \author
+   Ang Jun Sheng Aloysius
+  \brief
+   This function checks for the collision in a rectangle.
+   object
+  \param[in] area_center_x
+   Center of the rectangle's X position
+  \param[in] area_center_y
+   Center of the rectangle's Y position
+  \param[in] area_width
+   Width of the rectangle
+  \param[in] area_height
+   Height of the rectangle
+  \param[in] click_x
+   X-coordinate of mouse cursor
+  \param[in] click_y
+   Y-coordinate of mouse cursor
+  \return
+   1 if true, 0 if false
+********************************************************************/
 int IsAreaClicked(float area_center_x, float area_center_y, float area_width, float area_height, int click_x, int click_y)
 {
 	// TODO
@@ -154,21 +174,46 @@ int IsAreaClicked(float area_center_x, float area_center_y, float area_width, fl
 	return 0;
 }
 
-// Function to get window width
+/*!*****************************************************************
+  \author
+   Ang Jun Sheng Aloysius
+  \brief
+   This function gets the width of the window.
+  \param[in] none
+  \return
+   Width of the window
+********************************************************************/
 float getWinWidth() {
 	return AEGfxGetWinMaxX() - AEGfxGetWinMinX();
 }
 
-// Function to get window width
+/*!*****************************************************************
+  \author
+   Ang Jun Sheng Aloysius
+  \brief
+   This function gets the height of the window.
+  \param[in] none
+  \return
+   Height of the window
+********************************************************************/
 float getWinHeight() {
 	return AEGfxGetWinMaxY() - AEGfxGetWinMinY();
 }
 
-// Function to get angle of cursor to player in radian
+/*!*****************************************************************
+\author
+Lim Zhan Peng
+\brief
+This function returns the angle between the cursor and player drone
+\param[in] pos
+Player position
+\return
+angle in radian
+********************************************************************/
 double getCursorRad(AEVec2 pos) {
-    s32 cX, cY; //Mouse pos
-    double rad = 0; //radian value to be returned
-    const double pi = 3.1415926; //define pi
+    s32 cX, cY;                     //Mouse position
+    double rad = 0;                 //radian value to be returned
+    const double pi = 3.1415926;    //define pi
     float sumx = 0, sumy = 0;
     pos.y += 75;
 
@@ -180,10 +225,10 @@ double getCursorRad(AEVec2 pos) {
     sumy = MP.y - pos.y;
 
     //for each quadrant
-    if (MP.x > pos.x && MP.y > pos.y) { rad = pi + atan(sumy / sumx); }                  //Quad1
+    if (MP.x > pos.x && MP.y > pos.y) { rad = pi + atan(sumy / sumx); }          //Quad1
     else if (MP.x < pos.x && MP.y > pos.y) { rad = pi + atan(sumy / sumx); }     //Quad2
     else if (MP.x < pos.x && MP.y < pos.y) { rad = pi + atan(sumy / sumx); }     //Quad3
-    else if (MP.x > pos.x && MP.y < pos.y) { rad = pi + atan(sumy / sumx); }             //Quad4
+    else if (MP.x > pos.x && MP.y < pos.y) { rad = pi + atan(sumy / sumx); }     //Quad4
 
     return rad; //Return radian value
 }
--- SEP2_Project/SEP2_Project/Utilities.h
+++ SEP2_Project/SEP2_Project/Utilities.h
@@ -46,14 +46,59 @@ bool CollisionIntersection_RectRect(const AABB& aabb1, const AEVec2& vel1,
 
 bool CollisionCircleCircle(const AEVec2& center1, f32 radius1, const AEVec2& center2, f32 radius2);
 
-// Function to check if the area is being touched/clicked
+/*!*****************************************************************
+  \author
+   Ang Jun Sheng Aloysius
+  \brief
+   This function checks for the collision in a rectangle.
+   object
+  \param[in] area_center_x
+   Center of the rectangle's X position
+  \param[in] area_center_y
+   Center of the rectangle's Y position
+  \param[in] area_width
+   Width of the rectangle
+  \param[in] area_height
+   Height of the rectangle
+  \param[in] click_x
+   X-coordinate of mouse cursor
+  \param[in] click_y
+   Y-coordinate of mouse cursor
+  \return
+   1 if true, 0 if false
+********************************************************************/
 int IsAreaClicked(float area_center_x, float area_center_y, float area_width, float area_height, int click_x, int click_y);
 
-// Function to get window width
+/*!*****************************************************************
+  \author
+   Ang Jun Sheng Aloysius
+  \brief
+   This function gets the width of the window.
+  \param[in] none
+  \return
+   Width of the window
+********************************************************************/
 float getWinWidth();
 
-// Function to get window width
+/*!*****************************************************************
+  \author
+   Ang Jun Sheng Aloysius
+  \brief
+   This function gets the height of the window.
+  \param[in] none
+  \return
+   Height of the window
+********************************************************************/
 float getWinHeight();
 
-// Function to get angle of cursor to player in radian
+/*!*****************************************************************
+\author
+Lim Zhan Peng
+\brief
+This function returns the angle between the cursor and player drone
+\param[in] pos
+Player position
+\return
+angle in radian
+********************************************************************/
 double getCursorRad(AEVec2 pos);
