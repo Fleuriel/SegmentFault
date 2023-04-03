c93d223fb64f1b893216363a9bd2c2d864d384e4
EmeryQuek <c.quek@digipen.edu>
2023-04-03 21:59:59 +0800

    documentation

--- SEP2_Project/SEP2_Project/Upgrade.h
+++ SEP2_Project/SEP2_Project/Upgrade.h
@@ -1,3 +1,16 @@
+/******************************************************************************/
+/*!
+\file		Upgrade.h
+\author		Emery Quek Cheng Kwang, c.quek, 2201955
+\par		c.quek@digipen.edu
+\brief		This is a header file for Upgrade.cpp which contain function declarations
+
+Copyright (C) 2023 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+ */
+ /******************************************************************************/
 #pragma once
 
 
